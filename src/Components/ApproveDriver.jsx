81299f45765803aa21d2373f06a50f2b34dac21c
LahiruC98 <lahiruc@evokelabs.io>
2024-08-14 02:36:08 +0530

    Documents see and approve driver

--- src/Components/ApproveDriver.jsx
+++ src/Components/ApproveDriver.jsx
@@ -1,44 +1,57 @@
-import {Button} from "react-bootstrap";
-import {getDatabase, remove, ref, set} from "firebase/database";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import app from "../FirebaseConfig";
+// import { Button } from "react-bootstrap";
+// import { getDatabase, ref, update } from "firebase/database";
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import app from "../FirebaseConfig";
 
-export default function ApproveDriver({driver, setIsVisible}) {
-    const db = getDatabase(app)
-    const password = 'qwer1234'
-    const auth = getAuth();
+// export default function ApproveDriver({ driver, setIsVisible }) {
+//   const db = getDatabase(app);
+//   const password = "qwer1234";
+//   const auth = getAuth();
 
-    // console.log(driver)
-    const Approve = async () => {
-        createUserWithEmailAndPassword(auth, driver.email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                const newDriverRef = ref(db, 'users/' + user.uid + '/')
-                set(newDriverRef,{
-                    email:driver.email,
-                    firstName:driver.firstName,
-                    lastName:driver.lastName,
-                    mobile:driver.mobile,
-                    nic:driver.nic,
-                    role:'driver'
-                })
-            }).then(async () => {
-                const removeRef = ref(db, `pendingDrivers/${driver.driverId}`)
-                await remove(removeRef)
-                setIsVisible(false)
-            alert('driver approved !')
-        })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode + errorMessage)
-                // ..
-            });
-    }
+//   console.log(driver)
+//   const Approve = async () => {
+//       createUserWithEmailAndPassword(auth, driver.email, password)
+//           .then((userCredential) => {
+//               const user = userCredential.user;
+//               const newDriverRef = ref(db, 'users/' + user.uid + '/')
+//               set(newDriverRef,{
+//                   email:driver.email,
+//                   firstName:driver.firstName,
+//                   lastName:driver.lastName,
+//                   mobile:driver.mobile,
+//                   nic:driver.nic,
+//                   role:'driver'
+//               })
+//           }).then(async () => {
+//               const removeRef = ref(db, `pendingDrivers/${driver.driverId}`)
+//               await remove(removeRef)
+//               setIsVisible(false)
+//           alert('driver approved !')
+//       })
+//           .catch((error) => {
+//               const errorCode = error.code;
+//               const errorMessage = error.message;
+//               console.log(errorCode + errorMessage)
+//               // ..
+//           });
+//   }
 
-    return(
-        <div>
-            <Button onClick={Approve}>Approve</Button>
-        </div>
-    )
-}
+//   const Approve = async () => {
+//     const driverRef = ref(db, `/users/${driver.id}`);
+//     try {
+//       await update(driverRef, {
+//         status: "approved",
+//       });
+//       console.log(`Driver with ID ${driver.id} approved.`);
+//       FetchPendingDriverData
+//     } catch (error) {
+//       console.error("Error updating driver status:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Button onClick={Approve}>Approve</Button>
+//     </div>
+//   );
+// }
--- src/Components/PendingDrivers.jsx
+++ src/Components/PendingDrivers.jsx
@@ -1,58 +1,130 @@
-import {Button, Card, Container} from "react-bootstrap";
-import {useEffect, useState} from "react";
-import {get, getDatabase, ref} from "firebase/database";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import { useState } from "react";
+import { get, getDatabase, ref, update } from "firebase/database";
 import app from "../FirebaseConfig";
-import ApproveDriver from "./ApproveDriver";
+import { Link } from "react-router-dom";
 
 export default function PendingDrivers() {
-    let [pendingDriverList, setPendingDriverList] = useState([])
-    let [isVisible , setIsVisible] = useState(true)
-    const db = getDatabase(app)
-    
-    async function FetchPendingDriverData() {
-        setIsVisible(true)
-        try{
-            const pendingDriverRef = ref(db,'pendingDrivers/')
-            const snapshot = await get(pendingDriverRef)
+  const [pendingDriverList, setPendingDriverList] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+  const db = getDatabase(app);
 
-            if(snapshot.exists()){
-                const driverIds = snapshot.val()
-                const temPendingDriverArray = Object.keys(driverIds).map(allDriverIds => {
-                    return {
-                        ...driverIds[allDriverIds],
-                        driverId: allDriverIds
-                    }
-                })
-                setPendingDriverList(temPendingDriverArray)
-            }
-
-        } catch (e) {
-            console.error('Error fetching data:', e);
-            return null; // Handle errors gracefully
-        }
+  const approve = async (driverId) => {
+    const driverRef = ref(db, `/users/${driverId}`);
+    try {
+      await update(driverRef, {
+        status: "approved",
+      });
+      console.log(`Driver with ID ${driverId} approved.`);
+      FetchPendingDriverData();
+    } catch (error) {
+      console.error("Error updating driver status:", error);
     }
+  };
 
-    return (
-        <div>
-            <Container>
-                <h3>Pending Driver requests</h3>
-                <Button onClick={FetchPendingDriverData}>Show Pending List</Button>
-                <div className="Row">
-                    {isVisible && (
-                        pendingDriverList.map((item, index) =>
-                            <Card key={index} className={'m-2 p-2 bg-glass'}>
-                                <Card.Title className={'mt-2'}>{item.firstName} {item.lastName}</Card.Title>
-                                <hr/>
-                                <Card.Text>Email : {item.email}</Card.Text>
-                                <Card.Text>Mobile : {item.mobile}</Card.Text>
-                                <Card.Text>NIC : {item.nic}</Card.Text>
-                                <ApproveDriver driver={item} setIsVisible={setIsVisible}></ApproveDriver>
-                            </Card>
-                        )
-                    )
-                    }
-                </div>
-            </Container>
-        </div>
-    )
+  async function FetchPendingDriverData() {
+    setIsVisible(true);
+    try {
+      const pendingDriverRef = ref(db, "/users");
+      const snapshot = await get(pendingDriverRef);
+
+      if (snapshot.exists()) {
+        const pendingDrivers = [];
+        snapshot.forEach((childSnapshot) => {
+          const userData = childSnapshot.val();
+          const userId = childSnapshot.key;
+
+          if (userData.role === "driver" && userData.status === "pending") {
+            pendingDrivers.push({ id: userId, ...userData });
+          }
+        });
+        setPendingDriverList(pendingDrivers);
+      } else {
+        console.log("No users found.");
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  }
+
+  return (
+    <div>
+      <Container>
+        <h3>Pending Driver Requests</h3>
+        <Button onClick={FetchPendingDriverData}>Show Pending List</Button>
+        {isVisible && (
+          <div className="Row">
+            {pendingDriverList.map((item, index) => (
+              <Card key={index} className={"m-2 p-5 bg-glass"}>
+                <Card.Title className={"mt-2"}>
+                  {item.firstName} {item.lastName}
+                </Card.Title>
+                <hr />
+                <Row>
+                  <Col xs={6}>
+                    <Card.Text>Email: {item.email}</Card.Text>
+                    <Card.Text>Mobile: {item.mobile}</Card.Text>
+                    <Card.Text>NIC: {item.nic}</Card.Text>
+                  </Col>
+                  <Col xs={6} className="d-flex">
+                    <Card.Body>
+                      <Card.Text>Driving License</Card.Text>
+                      <Image
+                        height={"100px"}
+                        width={"100px"}
+                        src={
+                          item.drivingLicenseUrl
+                            ? `${item.drivingLicenseUrl}.jpg`
+                            : ""
+                        }
+                        alt="Driving license"
+                      ></Image>
+                      <Link to={item.drivingLicenseUrl}>
+                        <Button variant="success" className="mt-2">
+                          Download
+                        </Button>
+                      </Link>
+                    </Card.Body>
+                    <Card.Body>
+                      <Card.Text>NIC Number</Card.Text>
+                      <Image
+                        height={"100px"}
+                        width={"100px"}
+                        src={item.nicFileUrl ? `${item.nicFileUrl}.jpg` : ""}
+                        alt="NIC"
+                      ></Image>
+                      <Link to={item.nicFileUrl}>
+                        <Button variant="success" className="mt-2">
+                          Download
+                        </Button>
+                      </Link>
+                    </Card.Body>
+                    <Card.Body>
+                      <Card.Text>Vehicle License</Card.Text>
+                      <Image
+                        height={"100px"}
+                        width={"100px"}
+                        src={
+                          item.vehicleRegistrationUrl
+                            ? `${item.vehicleRegistrationUrl}.jpg`
+                            : ""
+                        }
+                        alt="Vehicle license"
+                      ></Image>
+                      <Link to={item.vehicleRegistrationUrl}>
+                        <Button variant="success" className="mt-2">
+                          Download
+                        </Button>
+                      </Link>
+                    </Card.Body>
+                  </Col>
+                </Row>
+                <Button onClick={() => approve(item.id)}>Approve</Button>
+              </Card>
+            ))}
+          </div>
+        )}
+      </Container>
+    </div>
+  );
 }
